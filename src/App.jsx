bbdadbbb3caee15502f32469c43146ac8ff8cfb7
ahad30 <mohiminulislamahad@gmail.com>
2024-06-03 00:58:34 +0600

    dashboard implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,26 @@ import "./App.css";
 import { Toaster } from "react-hot-toast";
 import { routes } from "./Routes/routes";
 import AuthProvider from "./Providers/AuthProvider";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
     <AuthProvider>
+    <QueryClientProvider client={queryClient}>
     <HelmetProvider>
     <RouterProvider router={routes} />
     <Toaster />
     </HelmetProvider>
+    </QueryClientProvider>
     </AuthProvider>
     
-   
+  
     </>
   );
 }
